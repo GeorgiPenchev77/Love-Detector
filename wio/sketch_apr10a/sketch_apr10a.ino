0b7293aa8e1b2cb94b885ac9a59816cd02b3e1ef
Georgi Penchev <penchev@student.chalmers.se>
2024-04-16 15:23:41 +0200

    Implement the start-stop detection button
    Issue: #3

--- wio/sketch_apr10a/sketch_apr10a.ino
+++ wio/sketch_apr10a/sketch_apr10a.ino
@@ -1,25 +1,65 @@
 #include <TFT_eSPI.h>
+TFT_eSPI tft;
 
 #define STANDARD_HORIZONTAL_VIEW 3
+#define START BUTTON_3
+#define NEXT_Q BUTTON_2
+#define HELP BUTTON_1
 
-TFT_eSPI tft;
+String start_message ="Hello and Welcome to The Love Detector. Press the leftmost button to begin. While in test, press the middle button to change to the next question. Press the rightmost button for help.";
+String begin_message ="Heart rate test will begin in 5 seconds. Make sure the sensor is connected to your ear securely. If there is an issue you will be notified with a message.";
+String result_message = "Your heart rate is: ";
+String loading_message = "Measuring heart rate: This process will take 20 seconds. ";
+String error_message= "Heart rate measure error, test will restart!";
+String reset_message= "Test has been stopped. Press button again to reset the test.";
+
 
 unsigned long temp[21];
 unsigned char counter = 0;
 bool data_effect = true;
 unsigned int heart_rate;
 const int max_heartpluse_duty = 2000;
+volatile bool pressed_start= false;
+
+
+void printMessage(String string)
+{
+  tft.setCursor(0, 0);
+  tft.setTextColor(TFT_BLACK);
+  tft.setTextSize(2);
+  tft.println(string);
+}
+
+void clear()
+{
+  tft.fillScreen(TFT_WHITE);
+}
+
+void press()
+{
+  if(digitalRead(START)==LOW)
+  {
+    pressed_start=!pressed_start;
+  }
+}
+
+void reset()
+{
+  data_effect=0;
+  counter=0;
+}
 
 void setup() {
-  //Set up commands to display messages on the Wio screen
+  pinMode(START,INPUT);
   tft.begin();
-  tft.setRotation(STANDARD_HORIZONTAL_VIEW);
+  tft.setRotation(STANDARD_HORIZONTAL_VIEW); //Set up commands to display messages on the Wio screen
+  Serial.begin(9600);  //Open port to display messages in the Arduino IDE terminal
+
+  delay(1000);
+  printMessage(start_message);
 
   arrayInit();  //initialize the data array with 0s
-
-  Serial.begin(9600);  //Open port to display messages in the Arduino IDE terminal
-  delay(5000);
-  Serial.println("Heart rate test begin.");
+  attachInterrupt(digitalPinToInterrupt(START), press, CHANGE);
   attachInterrupt(digitalPinToInterrupt(0), interrupt, RISING);
   /*
       attachInterrupt(pin, ISR, mode) creates an "event listener", that triggers whenever specified event happens on the monitored pins.
@@ -31,35 +71,74 @@ void setup() {
 }
 
 void loop() {
+  if(pressed_start)
+  {
+    clear();
+    if(heart_rate==0)
+    {
+    printMessage(loading_message);
+    delay(5000);
+    }
+    else
+    {
+      printMessage(result_message+heart_rate);
+      delay(5000);
+    }  
+  }
+  else if(!pressed_start)
+  {
+    clear();
+    printMessage(reset_message);
+    delay(5000);
+  }
 }
 
-void interrupt() {
+void interrupt() 
+{
+  if(pressed_start)
+  {
   temp[counter] = millis();
 
-  switch (counter) {
+  switch (counter) 
+  {
     case 0:
       break;
     default:
       unsigned long sub = temp[counter] - temp[counter - 1];
-      if (sub > max_heartpluse_duty) {
+      if (sub > max_heartpluse_duty) 
+      {
         data_effect = false;
         counter = 0;
-        Serial.println("Heart rate measure error, test will restart!");
+        clear();
+        printMessage(error_message);
         arrayInit();
         return;
       }
       break;
   }
 
-  if (counter == 20 && data_effect) {
+  if (counter == 20 && data_effect) 
+  {
     counter = 0;
     sum();
-  } else if (counter != 20 && data_effect)
+  } 
+  else if (counter != 20 && data_effect)
+  {
     counter++;
-  else {
+  }
+  else 
+  {
     counter = 0;
     data_effect = true;
   }
+
+  }
+
+  else if(!pressed_start)
+  {
+    reset();
+  }
+
 }
 
 void arrayInit() {
@@ -70,16 +149,9 @@ void arrayInit() {
 }
 
 void sum() {
-  if (data_effect) {
+  if (data_effect) 
+  {
     heart_rate = 1200000 / (temp[20] - temp[0]);
-    tft.fillScreen(TFT_BLACK);
-    tft.setCursor(0, 0);
-    tft.setTextColor(TFT_WHITE);
-    tft.setTextSize(2);
-    tft.print("Heart Rate: ");
-    tft.println(heart_rate);
-    Serial.println(heart_rate);
-    //delay(5000);
   }
   data_effect = true;
 }
