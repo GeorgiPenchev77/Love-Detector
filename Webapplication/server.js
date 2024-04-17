33c84c9edf2dfa3f52ece006fc2b093d70dcc6e0
gustramkr@student.gu.se <gustramkr@student.gu.se>
2024-04-17 20:15:36 +0200

    modify routing

--- Webapplication/server.js
+++ Webapplication/server.js
@@ -1,23 +1,11 @@
-import express from "express";
-import path from 'path';
-
+const express = require('express');
 const app = express();
-const port = 3000;
-
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-app.use(express.static(path.join(__dirname, 'public')));
 
 
-// Define your route handlers
-const router = express.Router();
+app.use(express.static('public')); // Serve static files from the 'public' directory
 
-app.use('/', router);
-
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`);
+// Start the server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
