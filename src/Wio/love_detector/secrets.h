86fdb01a7ca5083f7458982a16bf8a546452150e
Pavlo Ovchynnykov <gusovchpa@student.gu.se>
2024-04-26 16:33:59 +0200

    #9: Update .gitlab-ci.yml

--- src/Wio/love_detector/secrets.h
+++
@@ -1,3 +0,0 @@
-#define SECRET_SSID "NETWORK_NAME"      //Change to your local network name
-#define SECRET_PASS "PASSWORD"          //Change to your network's password
-#define SECRET_IP "127.0.0.1"           //Change to the address of the broker
