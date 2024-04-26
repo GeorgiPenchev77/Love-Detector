b9afef48cb0cdf5a4fe8118aad91b8008327f951
Georgi Penchev <penchev@student.chalmers.se>
2024-04-26 16:20:47 +0200

    #15 : implement minor code improvements
     Added some comments, uncommented some lines and refactored code.
     Fixed a bug where printing of heartbeats would
     become desyncronized if users have different
     heartbeats.

--- src/Wio/love_detector/love_detector.ino
+++ src/Wio/love_detector/love_detector.ino
@@ -2,17 +2,22 @@
 #include "secrets.h"
 TFT_eSPI tft;
 
+//--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------//
 
 #define STANDARD_HORIZONTAL_VIEW 3
 //define the three buttons of the terminal: 3 is left, 2 is middle, 1 is right
 #define START BUTTON_3
 #define NEXT_Q BUTTON_2
 #define HELP BUTTON_1
-#define SENSOR1 PIN_WIRE_SCL
-#define SENSOR2 0
+
+#define SENSOR1 PIN_WIRE_SCL //define the left pin of the terminal as Sensor 1 
+#define SENSOR2 0 //define the right pin of the terminal as Sensor 2
+
+//--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------//
 
 const String start_message = "Hello and Welcome to The Love Detector.\n Press the left button to begin. While in test, press the left button to stop the test or the middle button to change to the next question. Press the right button for help.";
-const String result_message = "Your heart rate is: ";
+const String result_message1 = "Heart rate of left user is: ";
+const String result_message2 = "Heart rate of right user is: ";
 const String loading_message = "Heart rate test has begun.\n You will be notified when it is complete or if there is an issue.";
 const String error_message1 = "Heart rate1 measure error, test will restart automatically.\n Make sure the sensor is attached securely!";
 const String error_message2 = "Heart rate2 measure error, test will restart automatically.\n Make sure the sensor is attached securely!";
@@ -20,35 +25,37 @@ const String reset_message = "Test has been stopped.\n Press button again to res
 
 const int max_heartpluse_duty = 2000;  //maximum delay between sensor logs, i.e. if >2 seconds there will be an error pop-up
 
-bool previous_state = false;
+volatile bool previous_state = false; //boolean to store the previous state of the program
 volatile bool is_started = false;  // boolean to store whether the test has been started or not (this is the via BUTTON_3)
 
+bool updated1 = false, updated2 = false; 
+
 //all used variables are duplicated in order to get the second sensor working
-unsigned long sub1,sub2;
-unsigned long temp1[21],temp2[21];
-unsigned char counter1,counter2;
-unsigned int heart_rate1,heart_rate2;
+unsigned long sub1, sub2;
+unsigned long temp1[21], temp2[21];
+unsigned char counter1, counter2;
+unsigned int heart_rate1 = 0, heart_rate2 = 0;
 volatile bool data_effect1 = true, data_effect2 = true;  // boolean to store whether data is valid or not
 
 //--------------------------------------------------------------------------------------------------------------------------------------------------------------//
 
 //please enter your sensitive data in the Secret tab/arduino_secrets.h
-/*char ssid[] = SECRET_SSID;        // your network SSID (name)
-char pass[] = SECRET_PASS;    // your network password (use for WPA, or use as key for WEP)
+char ssid[] = SECRET_SSID;  // your network SSID (name)
+char pass[] = SECRET_PASS;  // your network password (use for WPA, or use as key for WEP)
 
 WiFiClient wifiClient;
 MqttClient mqttClient(wifiClient);
 
 const char broker[] = "192.168.0.130";
-int        port     = 1883;
-const char topic[]  = "test";
-const char topic2[]  = "real_unique_topic_2";
-const char topic3[]  = "real_unique_topic_3";
+int port = 1883;
+const char topic[] = "test";
+const char topic2[] = "real_unique_topic_2";
+const char topic3[] = "real_unique_topic_3";
 
 //set interval for sending messages (milliseconds)
 const long interval = 8000;
 unsigned long previousMillis = 0;
-*/
+
 
 
 //--------------------------------------------------------------------------------------------------------------------------------------------------------------//
@@ -77,32 +84,33 @@ void reset1() {
   counter1 = 0;
 }
 
-void reset2(){
+void reset2() {
   data_effect2 = 0;
   counter2 = 0;
 }
 
+//--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------//
 
 void setup() {
-  pinMode(SENSOR1, INPUT); //Define the analog/digital port as a digital port (left port)
-  pinMode(SENSOR2, INPUT); //Define the right port
-  pinMode(START, INPUT); //initialize the button as an input device
+  pinMode(SENSOR1, INPUT);  //Define the analog/digital port as a digital port (left port)
+  pinMode(SENSOR2, INPUT);  //Define the right port
+  pinMode(START, INPUT);    //initialize the button as an input device
   tft.begin();
   tft.setRotation(STANDARD_HORIZONTAL_VIEW);  //Set up commands to display messages on the Wio screen
 
- /* WiFi.mode(WIFI_STA);
+  WiFi.mode(WIFI_STA);
   WiFi.disconnect();
 
-  printNewMessage(10,10,"Attempting to connect to ");
-  printNewMessage(100,100,ssid);
-  
+  printNewMessage(10, 10, "Attempting to connect to ");
+  printNewMessage(100, 100, ssid);
+
   while (WiFi.begin(ssid, pass) != WL_CONNECTED) {
     // failed, retry
     tft.println(".");
     delay(5000);
   }
 
-  printNewMessage(200,200,"Connection successful.");
+  printNewMessage(200, 200, "Connection successful.");
 
   delay(2000);
 
@@ -114,7 +122,8 @@ void setup() {
     Serial.print("MQTT connection failed! Error code = ");
     Serial.println(mqttClient.connectError());
 
-    while (1);
+    while (1)
+      ;
   }
 
   printNewMessage("You're connected to the MQTT broker!");
@@ -124,12 +133,13 @@ void setup() {
   mqttClient.beginMessage(topic);
   mqttClient.print("test message from Wio");
   mqttClient.endMessage();
-*/
 
-  printNewMessage(10,10,start_message);
 
-  arrayInit1();  //initialize the data array with 0s
+  printNewMessage(10, 10, start_message);
+
   arrayInit2();
+  arrayInit1();
+  delay(1000);
   /*
       attachInterrupt(pin, ISR, mode) creates an "event listener", that triggers whenever specified event happens on the monitored pins.
       In our case:
@@ -137,28 +147,43 @@ void setup() {
         - "interrupt" is the name of the callback function, called when a change occurs
         - "RISING" specifies that we only need to call the function when the pin goes from LOW to HIGH
   */
-  attachInterrupt(SENSOR1 , interrupt1, RISING);
+  attachInterrupt(SENSOR1, interrupt1, RISING);
   attachInterrupt(SENSOR2, interrupt2, RISING);
   attachInterrupt(START, press, CHANGE);
 }
 
+//--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------//
+
 //the loop will only change if there is a button press
 void loop() {
 
-  //mqttClient.poll();
+  mqttClient.poll();
   if (is_started != previous_state) {
     if (is_started) {
       clear();
-      printNewMessage(10,10,loading_message);
-
+      printNewMessage(10, 10, loading_message);
     } else if (!is_started) {
       clear();
-      printNewMessage(10,10, reset_message);
+      printNewMessage(10, 10, reset_message);
     }
-
-    previous_state = is_started;
   }
+
+  if (is_started) {
+    if (updated1 == true && updated2 == true) {
+      clear();
+      printNewMessage(10, 10, result_message1 + String(heart_rate1));
+      printNewMessage(100, 150, result_message2 + String(heart_rate2));
+      updated1 = false;
+      updated2 = false;
+    }
+  }
+
+  previous_state = is_started;
 }
+
+
+//--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------//
+//From here on all functions are doubled to account for the two sensors working in the system
 
 /* interrupt function is stopped via button press,
 when stopped the test will be marked as invalid and reset to run again,
@@ -169,27 +194,22 @@ void interrupt1() {
   if (is_started) {
     temp1[counter1] = millis();
 
-    switch (counter1) 
-    {
+    switch (counter1) {
       case 0:
-        sub1 = temp1[counter1] - temp1[20];
         break;
       default:
         sub1 = temp1[counter1] - temp1[counter1 - 1];
-        //Serial.println(sub);
         break;
     }
-        
-        if (sub1 > max_heartpluse_duty) 
-        {
-          reset1();
-          clear();
-          printNewMessage(10,10,error_message1);
-          arrayInit1();
-        }
 
-    if (counter1 == 20 && data_effect1) 
-    {
+    if (sub1 > max_heartpluse_duty) {
+      reset1();
+      clear();
+      printNewMessage(10, 10, error_message1);
+      arrayInit1();
+    }
+
+    if (counter1 == 20 && data_effect1) {
       counter1 = 0;
       sum1();
     } else if (counter1 != 20 && data_effect1) {
@@ -211,28 +231,23 @@ void interrupt2() {
   if (is_started) {
     temp2[counter2] = millis();
 
-    switch (counter2) 
-    {
+    switch (counter2) {
       case 0:
-        sub2 = temp2[counter2]-temp2[20];
         break;
       default:
         sub2 = temp2[counter2] - temp2[counter2 - 1];
-        //Serial.println(sub2);
         break;
     }
-        
-        
-        if (sub2 > max_heartpluse_duty) 
-        {
-          reset2();
-          clear();
-          printNewMessage(10,10,error_message2);
-          arrayInit2();
-        }
 
-    if (counter2 == 20 && data_effect2) 
-    {
+
+    if (sub2 > max_heartpluse_duty) {
+      reset2();
+      clear();
+      printNewMessage(10, 10, error_message2);
+      arrayInit2();
+    }
+
+    if (counter2 == 20 && data_effect2) {
       counter2 = 0;
       sum2();
     } else if (counter2 != 20 && data_effect2) {
@@ -268,8 +283,7 @@ void arrayInit2() {
 void sum1() {
   if (data_effect1) {
     heart_rate1 = 1200000 / (temp1[20] - temp1[0]);
-    clear();
-    printNewMessage(10,10, result_message + String(heart_rate1));
+    updated1 = true;
   }
   data_effect1 = true;
 }
@@ -277,10 +291,7 @@ void sum1() {
 void sum2() {
   if (data_effect2) {
     heart_rate2 = 1200000 / (temp2[20] - temp2[0]);
-    printNewMessage(150,150, result_message + String(heart_rate2));
+    updated2 = true;
   }
   data_effect2 = true;
 }
-
-
-
