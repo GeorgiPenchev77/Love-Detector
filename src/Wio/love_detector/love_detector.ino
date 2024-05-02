e0e2b4185791bc2a0636fff18fa7e726b7c54b75
René Ignacio González Pérez <renei@student.chalmers.se>
2024-05-02 18:58:46 +0200

    #21: Add normal mode, small cleanup

--- src/Wio/love_detector/love_detector.ino
+++ src/Wio/love_detector/love_detector.ino
@@ -252,14 +252,9 @@ void sum() {
   }
   data_effect = true;
 }
-void alterningBlink (uint32_t color) {
-  for (int j = 0; j < 10; j++) {
-    pixels.setPixelColor(j, color);
-  }
-}
 
-void blinkSequence(uint32_t color){
-for (int j = 0; j < 2; j++) { // makes it on off then on 2 times, with small delay
+void blinkSequence(uint32_t color){// makes it on off then on 2 times, with small delay
+for (int j = 0; j < 2; j++) { 
     for (int i = 0; i < NUMPIXELS; i++) {
       pixels.setPixelColor(i, color);
     }
@@ -267,12 +262,10 @@ for (int j = 0; j < 2; j++) { // makes it on off then on 2 times, with small del
     delay(200); 
     pixels.clear(); 
     pixels.show(); 
-    delay(200); 
   }
 }
-void lightUpSequence(uint32_t color) {
-  
-  for (int i = 0; i < NUMPIXELS; i++) {//light up LEDS one by one
+void lightUpSequence(uint32_t color) {//lights up LEDS one by one
+  for (int i = 0; i < NUMPIXELS; i++) {
     pixels.setPixelColor(i, color);
     pixels.show();
     delay(250); // Delay for the build up
@@ -280,44 +273,35 @@ void lightUpSequence(uint32_t color) {
   delay(500);
   pixels.clear(); 
 }
-
-
-void fadeInSequence() {
- //Fading effect
+void fadeInSequence() {//Fading effect only blue
     for (int b = 0; b < 256; b++) {
       for (int i = 0; i < NUMPIXELS; i++) {
-        pixels.setPixelColor(i, pixels.Color(b-1, 0, b/2)); // Fade in
+        pixels.setPixelColor(i, pixels.Color(0, b, b)); // Fade in blue
       }
       pixels.show();
       delay(5); 
     }
-
-    delay(1000); 
-
-      for (int j = 0; j < 5; j++) { // 5 blink cycles
-      pixels.clear(); 
-      pixels.show(); 
-      delay(200); 
-      for (int i = 0; i < NUMPIXELS; i++) {
-        pixels.setPixelColor(i, pixels.Color(255, 0, 127));
-      }
-      pixels.show(); 
-      delay(200); 
-    }
-
-    delay(500); 
 }
 void upAndDown() { // function for the lights fillup and down 
-    for (int i = 0; i < NUMPIXELS; i++) { 
-        pixels.setPixelColor(i, pixels.Color(255, 0, 127)); //pink color
-        pixels.show();
-        delay(25);
-      }
-      for (int i = 10; i >= 0; i--) {
-        pixels.setPixelColor(i, pixels.Color(255, 255, 168)); //brighter color
-        pixels.show();
-        delay(25);
-      }
+  for (int i = 0; i < NUMPIXELS; i++) { 
+      pixels.setPixelColor(i, pixels.Color(255, 0, 127)); //pink color
+      pixels.show();
+      delay(25);
+    }
+    for (int i = 10; i >= 0; i--) {
+      pixels.setPixelColor(i, pixels.Color(255, 255, 168)); //brighter color
+      pixels.show();
+      delay(25);
+    }
+}
+
+void normalMode(){
+  color = pixels.Color(0, 0, 255);
+  lightUpSequence(color);
+  delay(100);
+  blinkSequence(color);
+  delay(100);
+  fadeInSequence();
 }
 void crazyMode() { // for ultimate love match
   uint32_t color = pixels.Color(255, 0, 127); // pink color for the blinking
@@ -335,11 +319,9 @@ void light(int level) {
       delay(1500); // The amount of time between each individually lit up pixel
     }
   } else if (level > 25 && level <= 75) {
-    color = pixels.Color(0, 0, 255);
-    lightUpSequence(color);
-    delay(100);
-    blinkSequence(color);
-    delay(100);
+    for (int i = 0; i < level; i++) {
+      normalMode();
+   }
   } else {
     for (int i = 0; i < level; i++) {
       crazyMode();
